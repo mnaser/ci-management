38fe8bea7e99bde4bae1ed074f6b832a078fb5fb
Mohammed Naser <mnaser@vexxhost.com>
2019-02-15 09:19:29 -0500

    test: fix checkstyle

--- jjb/vpp/include-raw-vpp-checkstyle.sh
+++ jjb/vpp/include-raw-vpp-checkstyle.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-if [ -f build-root/scripts/checkstyle.sh ];then
-    build-root/scripts/checkstyle.sh
-else
-    echo "Cannot find cat build-root/scripts/checkstyle.sh - skipping checkstyle"
-fi
+exit 1
